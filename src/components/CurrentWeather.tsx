082fb9a6658334ed56b20d08581224cc82f3c9a7
Drashti-Patel <drashti.jasani@solita.fi>
2023-10-21 11:05:14 +0300

    add current weather component

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 const CurrentWeather: React.FC = () => {
+
+const [data, setData] = useState({})
+const [location, setLocation] = useState('')
+
+const url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e8af103c2174f8bddee3dd5c2533733b"
+
+
   return (
-    <div className="w-full bg-red-500 shadow-md rounded-md">
-      Add current weather here
+    <div className="w-full bg-gray-400 shadow-md rounded-md h-50">
+    <section className="vh-100" style={{ backgroundColor: "#C1CFEA" }}>
+      <MDBContainer className="h-100">
+        <MDBRow
+          className="justify-content-center align-items-center h-100"
+          style={{ color: "#282828" }}
+        >
+          <MDBCol md="9" lg="7" xl="5">
+            <MDBCard
+              className="mb-4 gradient-custom"
+              style={{ borderRadius: "25px" }}
+            >
+              <MDBCardBody className="p-4">
+                <div className="d-flex justify-content-between pb-2">
+                  <div>
+                    <h2 className="display-2">
+                      <strong>23°C</strong>
+                    </h2>
+                    <p className="text-muted mb-0">Coimbra, Portugal</p>
+                  </div>
+                  <div>
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu3.webp alt-text="
+                      alt="weather-info"
+                      width="150px"
+                    />
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
     </div>
   );
 };
