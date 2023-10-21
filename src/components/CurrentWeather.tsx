938a3b140bb1e76220683eef54197da188259648
Drashti-Patel <drashti.jasani@solita.fi>
2023-10-21 23:52:23 +0300

    - add weather component

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,57 +1,33 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCol,
-  MDBContainer,
-  MDBRow,
-} from "mdb-react-ui-kit";
+import sunImage from '../assets/sun-svgrepo-com.svg'; // Import the image
 
 const CurrentWeather: React.FC = () => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
-const [data, setData] = useState({})
-const [location, setLocation] = useState('')
-
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e8af103c2174f8bddee3dd5c2533733b"
-
+  const url =
+    'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e8af103c2174f8bddee3dd5c2533733b';
 
   return (
-    <div className="w-full bg-gray-400 shadow-md rounded-md h-50">
-    <section className="vh-100" style={{ backgroundColor: "#C1CFEA" }}>
-      <MDBContainer className="h-100">
-        <MDBRow
-          className="justify-content-center align-items-center h-100"
-          style={{ color: "#282828" }}
-        >
-          <MDBCol md="9" lg="7" xl="5">
-            <MDBCard
-              className="mb-4 gradient-custom"
-              style={{ borderRadius: "25px" }}
-            >
-              <MDBCardBody className="p-4">
-                <div className="d-flex justify-content-between pb-2">
-                  <div>
-                    <h2 className="display-2">
-                      <strong>23°C</strong>
-                    </h2>
-                    <p className="text-muted mb-0">Coimbra, Portugal</p>
-                  </div>
-                  <div>
-                    <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu3.webp alt-text="
-                      alt="weather-info"
-                      width="150px"
-                    />
-                  </div>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </section>
+    <div className="w-full bg-gray-400 shadow-md rounded-md grid grid-cols-2 ">
+      <div className="grid grid-rows-2 grid-flow-col bg-red-400 col-span-1">
+        <div className="bg-indigo-500">
+          <div className="flex flex-col items-start ml-3 mt-5">
+            <h1>Madrid</h1>
+
+            <h5>chance of rain 0%</h5>
+          </div>
+        </div>
+
+        <div className="w-full h-full bg-orange-800 flex flex-col justify-center items-start">
+          <h1 className="ml-3">31°</h1>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <img src={sunImage} alt="temperature" width={150} height={150}/>
+      </div>
     </div>
   );
 };
