fe8038be47d555db6499f8da0dd66c6d5f9af342
Drashti-Patel <drashti.jasani@solita.fi>
2023-10-31 15:23:39 +0200

    current weather component update

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -3,11 +3,21 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import sunImage from '../assets/sun-svgrepo-com.svg'; // Import the image
 
-const CurrentWeather: React.FC = () => {
-  const [data, setData] = useState(null);
+interface CurrentWeatherInfo {
+  name: string
+  main : {
+    temp: string,
+    feels_like: string
+  }
+  weather: [
+    {
+      description: string
+    }
+  ]
+}
 
-  // const url = 
-  //   'https://api.openweathermap.org/data/3.0/onecall?lat=62.2447&lon=25.7473&exclude={part}&appid=e8af103c2174f8bddee3dd5c2533733b';
+const CurrentWeather: React.FC = () => {
+  const [data, setData] = useState<CurrentWeatherInfo>();
 
     const fetchData = async () => {
       try {
@@ -15,7 +25,6 @@ const CurrentWeather: React.FC = () => {
           'https://api.openweathermap.org/data/2.5/weather?q=jyväskylä&units=imperial&appid=895284fb2d2c50a520ea537456963d9c'
           );
         setData(response.data);
-        console.log(response.data); //You can see all the weather data in console log
       } catch (error) {
         console.error(error);
       }
@@ -28,19 +37,20 @@ const CurrentWeather: React.FC = () => {
 
   return (
     <div className="w-full bg-black shadow-md rounded-md grid grid-cols-2 ">
-      <div className="grid grid-rows-2 grid-flow-col bg-red-400 col-span-1">
+      <div className="grid grid-rows-2 grid-flow-col bg-black-400 col-span-1">
         <div className="bg-black">
           <div className="flex flex-col items-start ml-3 mt-5 text-white">
-            <h1>Madrid</h1>
+            <h1>{data? data.name : "Madrid"}</h1>
 
             <div className="text-gray-400">
-            <h5>chance of rain 0%</h5>
+            {data ? <h5>{data.weather ? "Weather: "+data.weather[0].description : "snow"}</h5> : null}
             </div>
           </div>
         </div>
 
-        <div className="w-full h-full bg-black flex flex-col justify-center items-start text-white">
-        {/* {data ? <h1>{data.name}°F</h1> : null} */}
+        <div className="w-full h-full ml-3 bg-black flex flex-col justify-center items-start text-white">
+        {data ? <h1>{data.main ? "Temprature: "+data.main.temp : "5"}°F</h1> : null}
+        {data ? <h1>{data.main ? "Feels like: "+data.main.feels_like : "7"}°F</h1> : null}
         </div>
       </div>
       <div className="flex flex-col justify-center items-center">
